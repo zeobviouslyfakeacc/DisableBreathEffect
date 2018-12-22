342c53b35b183bde6eb39438c9dc342e41552f2c
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-12-22 16:54:25 +0100

    Cache method, improve code style

--- DisableBreathEffect.cs
+++ DisableBreathEffect.cs
@@ -1,27 +1,29 @@
 ﻿using System;
+using System.Reflection;
 using Harmony;
 
 [HarmonyPatch(typeof(Breath), "PlayBreathEffect", new Type[0])]
-public class DisableBreathEffect
-{
-	public static bool Prefix(Breath __instance)
-	{
-		bool suppress = (bool) AccessTools.Method(typeof(Breath), "ShouldSuppressBreathEffect").Invoke(__instance, new object[0]);
-		if (suppress) return false;
+internal static class DisableBreathEffect {
 
-		HeavyBreathingState heavyBreathingState = GameManager.GetFatigueComponent().GetHeavyBreathingState();
-		if (heavyBreathingState == HeavyBreathingState.Light)
-		{
-			GameAudioManager.PlaySound(AK.EVENTS.PLAY_VOBREATHELOWINTENSITYNOLOOP, GameManager.GetPlayerObject());
+	private static readonly MethodInfo shouldSuppressBreathEffect = AccessTools.Method(typeof(Breath), "ShouldSuppressBreathEffect");
+
+	private static bool Prefix(Breath __instance) {
+		bool suppress = (bool) shouldSuppressBreathEffect.Invoke(__instance, new object[0]);
+		if (suppress)
+			return false;
+
+		switch (GameManager.GetFatigueComponent().GetHeavyBreathingState()) {
+			case HeavyBreathingState.Light:
+				GameAudioManager.PlaySound(AK.EVENTS.PLAY_VOBREATHELOWINTENSITYNOLOOP, GameManager.GetPlayerObject());
+				break;
+			case HeavyBreathingState.Medium:
+				GameAudioManager.PlaySound(AK.EVENTS.PLAY_VOBREATHMEDIUMINTENSITYNOLOOP, GameManager.GetPlayerObject());
+				break;
+			case HeavyBreathingState.Heavy:
+				GameAudioManager.PlaySound(AK.EVENTS.PLAY_VOBREATHHIGHINTENSITYNOLOOP, GameManager.GetPlayerObject());
+				break;
 		}
-		else if (heavyBreathingState == HeavyBreathingState.Medium)
-		{
-			GameAudioManager.PlaySound(AK.EVENTS.PLAY_VOBREATHMEDIUMINTENSITYNOLOOP, GameManager.GetPlayerObject());
-		}
-		else if (heavyBreathingState == HeavyBreathingState.Heavy)
-		{
-			GameAudioManager.PlaySound(AK.EVENTS.PLAY_VOBREATHHIGHINTENSITYNOLOOP, GameManager.GetPlayerObject());
-		}
+
 		return false;
 	}
 }
